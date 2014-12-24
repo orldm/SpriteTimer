7361c7ea5b343d07ce1c1189cd23068c1f33408d
Dmitri <orldm@mail.ru>
2014-12-24 15:51:24 +0200

    add clock setting routine

--- SpriteTimer/GameScene.swift
+++ SpriteTimer/GameScene.swift
@@ -14,16 +14,18 @@ class GameScene: SKScene {
     var timeInterval: CFTimeInterval?
     let lineNode = SKSpriteNode()
     let squareNode = SKSpriteNode()
-    let angle = CGFloat(-M_PI/30.0)
+    var angle: Double = 0 //CGFloat(M_PI/30.0)
     let myLabel = SKLabelNode(fontNamed:"Helvetica")
-    var secondsCount = 0
+    var secondsCount: Int = 0
     var tickHappened = false
+    var clockIsSet = false
+    var settingIsEnabled = false
     
     override func didMoveToView(view: SKView) {
         
         //timeInterval = CF
         
-        myLabel.text = "00";
+        myLabel.text = "0";
         myLabel.fontSize = 65;
         myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
         self.addChild(myLabel)
@@ -45,46 +47,65 @@ class GameScene: SKScene {
         /* Called when a touch begins */
         
         for touch: AnyObject in touches {
-            let location = touch.locationInNode(self)
-            
-//            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-//            
-//            sprite.xScale = 0.5
-//            sprite.yScale = 0.5
-//            sprite.position = location
-//            
-//            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-//            
-//            sprite.runAction(SKAction.repeatActionForever(action))
-//            
-//            self.addChild(sprite)
+            let location = touch.locationInNode(lineNode)
+            if squareNode.containsPoint(location) {
+                
+                settingIsEnabled = true
+                clockIsSet = false
+
+            }
         }
     }
     
-   
+    override func touchesMoved(touches: NSSet, withEvent event: UIEvent) {
+        
+        if settingIsEnabled {
+        for touch: AnyObject in touches {
+            let location = touch.locationInNode(self)
+            
+                let moveArm = SKAction.reachTo(location, rootNode: lineNode, duration: 0.1)
+                squareNode.runAction(moveArm, completion: {
+                    self.angle = (Double(-self.lineNode.zRotation)/M_PI * 30)%60
+                    if self.angle < 0 { self.angle = 60 + self.angle }
+                    self.myLabel.text = "\(Int(self.angle))"
+                    self.secondsCount = Int(self.angle)
+                })
+            
+        }
+        }
+    }
+    
+    override func touchesEnded(touches: NSSet, withEvent event: UIEvent) {
+        settingIsEnabled = false
+        clockIsSet = true
+    }
+    
     override func update(currentTime: CFTimeInterval) {
         
-        if let timeInterval = timeInterval {
-            elapsedTime += currentTime - timeInterval
-        }
-        timeInterval = currentTime
-        
-        if elapsedTime >= 0.9 && !tickHappened {
-            lineNode.runAction(SKAction.rotateByAngle(angle, duration: 0.1))
-            tickHappened = true
-        }
-        
-        if elapsedTime >= 1.0 {
-            elapsedTime = 0.0
-            tickHappened = false
-            if secondsCount == 59 {
-                secondsCount = 0
-            } else {
-                secondsCount += 1
+        if clockIsSet {
+            if let timeInterval = timeInterval {
+                elapsedTime += currentTime - timeInterval
+            }
+            timeInterval = currentTime
+            
+            if elapsedTime >= 0.9 && !tickHappened {
+                let clockAngle = -CGFloat(secondsCount)*CGFloat(M_PI)/30
+                lineNode.runAction(SKAction.rotateToAngle(clockAngle, duration: 0.1, shortestUnitArc: true))
+                tickHappened = true
             }
             
-            myLabel.text = String(secondsCount)
-            
+            if elapsedTime >= 1.0 {
+                elapsedTime = 0.0
+                tickHappened = false
+                if secondsCount == 0 {
+                    myLabel.text = "0"
+                    clockIsSet = false
+                } else {
+                    secondsCount -= 1
+                    myLabel.text = String(secondsCount+1)
+                }
+                
+            }
         }
     }
 }
